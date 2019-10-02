60de1fabdc0da414f9bf00688661b43748092039
Alex-121 <adumas4.student.gsu.edu>
2019-10-01 23:10:27 -0400

    Chapter 7

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -18,7 +18,6 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Telephony;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
